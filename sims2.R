0ce341059e518dfc6e04e5d7a9a9d5d65bbad182
Adam Kapelner <kapelner@gmail.com>
2015-04-01 15:53:27 -0400

    file from sense.io

--- sims2.R
+++ sims2.R
@@ -1,17 +1,24 @@
-options(java.parameters = "-Xmx1000m")
+options(java.parameters = "-Xmx40000m")
+install.packages("GreedyExperimentalDesign_1.0.tar.gz", repos=NULL, type= "source", lib="~")
 library(GreedyExperimentalDesign)
 
-NUM_CORES = 4
+NUM_CORES = 31
 
-ns = c(100, 200, 400, 1000)
-ps = c(1, 5, 10, 30, 50)
-rs = c(1, 5, 10, 30, 100)
-DUPS = 15 #number of repeats per cell in the grid
+n= 100
+p = 1
+r = 1
+ns = c(1000)
+ps = c(1, 5)
+rs = c(1, 5, 30)
+DUPS = 25 #number of repeats per cell in the grid
 
 all_results = data.frame(matrix(NA, nrow = 0, ncol = 5))
 
 for (n in ns){
 	for (p in ps){
+		if (p >= n){
+			next
+		}
 		for (r in rs){
 			for (d in 1 : DUPS){
 				cat ("n =", n, "p =", p, "r =", r, "d =", d, "\n")
@@ -22,55 +29,38 @@ for (n in ns){
 				#get back the results
 				res = resultsGreedySearch(ged, max_vectors = 0)
 				#record the minimum balance found
-				all_results = rbind(all_results, c(n, p, r, d, res$obj_vals[1]))				
-			}					
+				all_results = rbind(all_results, c(n, p, r, d, res$obj_vals[1]))                
+			}                   
 		}
 		#record the results in a CSV iteratively
 		colnames(all_results) = c("n", "p", "r", "d", "val")
-		write.csv(all_results, "all_results.csv")			
+		write.csv(all_results, "all_results.csv")           
 	}
 }
 
 ### now analyze
 
--(1 + 2 / ps)
 library(data.table)
 X = fread("all_results.csv")
 dim(X)
 tail(X)
 X$invp = X$p^-1
-X$invr = X$r^-1
 
 #model time
-Xp = X[X$p == 1, ]
-mod = lm(log(val) ~ log(n), data = Xp)
+mod = lm(log(val) ~ log(n), data = X[X$p == 1, ])
 summary(mod)
-plot(log(Xp$n), log(Xp$val))
-abline(mod)
 
-Xp = X[X$p == 5, ]
-mod = lm(log(val) ~ log(n), data = Xp)
+mod = lm(log(val) ~ log(n), data = X[X$p == 5, ])
 summary(mod)
-plot(log(Xp$n), log(Xp$val))
-abline(mod)
 
-Xp = X[X$p == 10, ]
-mod = lm(log(val) ~ log(n), data = Xp)
+mod = lm(log(val) ~ log(n), data = X[X$p == 10, ])
 summary(mod)
-plot(log(Xp$n), log(Xp$val))
-abline(mod)
 
-Xp = X[X$p == 30, ]
-mod = lm(log(val) ~ log(n), data = Xp)
+mod = lm(log(val) ~ log(n), data = X[X$p == 20, ])
 summary(mod)
-plot(log(Xp$n), log(Xp$val))
-abline(mod)
 
-Xp = X[X$p == 50, ]
-mod = lm(log(val) ~ log(n), data = Xp)
+mod = lm(log(val) ~ log(n), data = X[X$p == 30, ])
 summary(mod)
-plot(log(Xp$n), log(Xp$val))
-abline(mod)
 
 mod = lm(log(val) ~ 0 + log(n) + log(n) : as.factor(p))
 summary(mod)
@@ -104,12 +94,8 @@ summary(mod4)
 mod5 = lm(log(val) ~ r * as.factor(p) + log(n) * as.factor(p) - as.factor(p) - log(n), data = X)
 summary(mod5)
 
-mod4 = lm(log(val) ~ log(r) * log(p)  + log(n) : as.factor(p), data = X)
+mod4 = lm(log(val) ~ log(n) * as.factor(p), data = X)
 summary(mod4)
-
-mod4 = lm(log(val) ~ poly(n, 2) * poly(r, 3) * poly(p, 2) + log(n) : as.factor(p), data = X)
-summary(mod4)
-
 
 mod1 = lm(log(val) ~ log(p) + log(n) * invp, data = X)
 summary(mod1)
@@ -119,7 +105,7 @@ summary(mod2)
 
 anova(mod1, mod2)
 
-mod8 = lm(log(val) ~ log(r) * log(p) * log(n), data = X)
+mod8 = lm(log(val) ~ log(r) + log(p) * log(n), data = X)
 summary(mod8)
 
 Xr = X[X$r == 3, ]
@@ -135,4 +121,3 @@ mod = lm(log(val) ~ log(n), data = Xpr)
 summary(mod)
 plot(log(Xpr$n), log(Xpr$val))
 abline(mod)
-
