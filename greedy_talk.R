da5101e403d3b5ea1baad0d9a13c3d13528658da
Adam Kapelner <kapelner@gmail.com>
2016-08-10 09:26:01 -0400

    plot changes from last year

--- greedy_talk.R
+++ greedy_talk.R
@@ -4,7 +4,7 @@ library(GreedyExperimentalDesign)
 
 NUM_CORES = 3
 
-ns = c(10, 100, 1000, 2000)
+ns = c(10, 100, 1000)
 ps = c(1, 2, 5, 10)
 x_reps = 5
 g_reps = 6
@@ -48,19 +48,21 @@ summary(mod_p_5)
 mod_p_10 = lm(greedy_obj_vals_avg_log[, 4] ~ log(ns))
 summary(mod_p_10)
 
-plot(log(ns), num_switches_avg[, 1], col = "blue", type = "o",
+manip_ns = ns
+
+plot(manip_ns, num_switches_avg[, 1], col = "blue", type = "o",
 		ylim = c(min(num_switches_avg), max(num_switches_avg)),
 		xlab = "n", ylab = "# switches", main = "#switches by n for p = 1, 2, 5, 10")
-points(log(ns), num_switches_avg[, 2], col = "red", type = "o")
-points(log(ns), num_switches_avg[, 3], col = "green", type = "o")
-points(log(ns), num_switches_avg[, 4], col = "purple", type = "o")
+points(manip_ns, num_switches_avg[, 2], col = "red", type = "o")
+points(manip_ns, num_switches_avg[, 3], col = "green", type = "o")
+points(manip_ns, num_switches_avg[, 4], col = "purple", type = "o")
 
-mod_p_1 = lm(num_switches_avg[, 1] ~ log(ns))
+mod_p_1 = lm(num_switches_avg[, 1] ~ manip_ns)
 summary(mod_p_1)
-mod_p_2 = lm(num_switches_avg[, 2] ~ log(ns))
+mod_p_2 = lm(num_switches_avg[, 2] ~ manip_ns)
 summary(mod_p_2)
-mod_p_5 = lm(num_switches_avg[, 3] ~ log(ns))
+mod_p_5 = lm(num_switches_avg[, 3] ~ manip_ns)
 summary(mod_p_5)
-mod_p_10 = lm(num_switches_avg[, 4] ~ log(ns))
+mod_p_10 = lm(num_switches_avg[, 4] ~ manip_ns)
 summary(mod_p_10)
 
