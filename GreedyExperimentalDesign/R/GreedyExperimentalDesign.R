aebefec37ef2e0c44dd99c21584f13a82ba7fbf0
Adam Kapelner <kapelner@gmail.com>
2018-08-19 22:36:43 +0300

    corrected problem with CRAN submission thanks to Uwe Ligges' suggestion

--- GreedyExperimentalDesign/R/GreedyExperimentalDesign.R
+++ GreedyExperimentalDesign/R/GreedyExperimentalDesign.R
@@ -6,7 +6,8 @@
 #' @author 		Adam Kapelner \email{kapelner@@qc.cuny.edu}
 #' @references 	Kapelner, A
 #' @keywords 	Experimental Design, Greedy search
-#' @import      rJava stats kernlab graphics grDevices GreedyExperimentalDesignJARs
+#' @import      rJava stats graphics grDevices GreedyExperimentalDesignJARs
+#' @importFrom kernlab vanilladot rbfdot polydot tanhdot besseldot laplacedot anovadot splinedot kernelMatrix
 ##### Run "library(roxygen2); roxygenise("GreedyExperimentalDesign", clean = TRUE)" to regenerate all Rd files and NAMESPACE and DESCRIPTION file
 ##### but make sure you are in the root directory of the project
 NULL
